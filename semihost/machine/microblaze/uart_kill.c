9b92dd575a8fc52ff5bce88164cc10521d568f2f
Alp Sayin <alpsayin@gmail.com>
2025-07-29 00:38:26 +0100

    Revert "microblaze: supress warning: no previous prototype for 'lseek64', 'kill'"
    This reverts commit 95800bd884cd1dd3f24bd8fc3bf1fd2432272c98.
    
    Signed-off-by: Alp Sayin <alpsayin@gmail.com>

--- semihost/machine/microblaze/uart_kill.c
+++ semihost/machine/microblaze/uart_kill.c
@@ -40,7 +40,4 @@
 
 pid_t getpid(void) { return 1; }
 
-#if 0
-/* suppress warning: no previous prototype for 'kill' */
 int kill(pid_t pid, int sig) { if (pid == 1) _exit(128 + sig); errno = ESRCH; return -1; }
-#endif
